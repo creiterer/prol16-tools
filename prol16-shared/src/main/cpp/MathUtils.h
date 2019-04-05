d8f54ee070a85058bd04a98b88ef9b23188a0070
creiterer <cr.devel0@gmail.com>
2019-04-05 13:10:30 +0200

    Issue #52: Refactor PROL16 VM
    * created math utilities

---
+++ prol16-shared/src/main/cpp/MathUtils.h
@@ -0,0 +1,32 @@
+/**
+ * @author		creiterer
+ * @date 		2019-04-02
+ * @copyright 	Copyright (c) 2019 Christopher Reiterer
+ * @brief 		brief description
+ */
+
+#ifndef PROL16_TOOLS_PROL16_SHARED_SRC_MAIN_CPP_MATHUTILS_H_INCLUDED
+#define PROL16_TOOLS_PROL16_SHARED_SRC_MAIN_CPP_MATHUTILS_H_INCLUDED
+
+#include <stdexcept>
+
+namespace util {
+
+/**
+ * Check if b is a multiple of a.
+ * @param a
+ * @param b
+ * @return	True if b = n*a, false otherwise.
+ */
+template <typename T>
+bool isMultiple(T const a, T const b) {
+	if (a == 0) {
+		throw std::invalid_argument("'a' is 0");
+	}
+
+	return (b % a) == 0;
+}
+
+}
+
+#endif /* PROL16_TOOLS_PROL16_SHARED_SRC_MAIN_CPP_MATHUTILS_H_INCLUDED */
---
+++ prol16-shared/src/test/cpp/MathUtilsTest.cpp
@@ -0,0 +1,24 @@
+/**
+ * @author		creiterer
+ * @date 		2019-04-02
+ * @copyright 	Copyright (c) 2019 Christopher Reiterer
+ * @brief 		brief description
+ */
+
+#include "MathUtils.h"
+
+#include <stdexcept>
+
+#include "gtest/gtest.h"
+
+using namespace util;
+
+TEST(MathUtilsTest, testIsMultipleOf) {
+	ASSERT_TRUE(isMultiple(2, 4));
+	ASSERT_TRUE(isMultiple(3, 9));
+
+	ASSERT_FALSE(isMultiple(3, 8));
+	ASSERT_FALSE(isMultiple(4, 19));
+
+	ASSERT_THROW(isMultiple(0, 1), std::invalid_argument);
+}
