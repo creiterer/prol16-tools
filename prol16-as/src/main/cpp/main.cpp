89e23ac976b15a9d1f5f1d33808b088b81534754
creiterer <cr.devel0@gmail.com>
2019-08-05 15:22:38 +0200

    Resolves #61: Use ArgumentParser for prol16-as

--- prol16-as/src/main/cpp/main.cpp
+++ prol16-as/src/main/cpp/main.cpp
@@ -21,29 +21,39 @@
 #include "ScopedFileStream.h"
 #include "Filename.h"
 #include "InstructionWriter.h"
+#include "ArgumentParser.h"
+#include "CLIArguments.h"
+#include "CLIError.h"
 
 using namespace antlr4;
 using std::cout;
 using std::cerr;
 using std::endl;
+using util::ScopedFileStream;
 
-static void printUsage(std::string const &errorMessage, std::string const &appName) {
-	cerr << errorMessage << endl;
-	cerr << "Usage: " << appName << " PROL16_ASSEMBLY_FILE" << endl;
-}
+//static void printUsage(std::string const &errorMessage, std::string const &appName) {
+//	cerr << errorMessage << endl;
+//	cerr << "Usage: " << appName << " PROL16_ASSEMBLY_FILE" << endl;
+//}
+
+static std::string const FILENAME_ARG_NAME = "PROL16_ASSEMBLY_FILE";
 
 int main(int const argc, char const * const argv[]) {
 	try {
-		if (argc != 2) {
-			std::ostringstream errorMessage;
-			errorMessage << "Error: expected exactly one PROL16 assembly input file but got " << (argc - 1);
-			printUsage(errorMessage.str(), argv[0]);
-			return 1;
-		}
+//		if (argc != 2) {
+//			std::ostringstream errorMessage;
+//			errorMessage << "Error: expected exactly one PROL16 assembly input file but got " << (argc - 1);
+//			printUsage(errorMessage.str(), argv[0]);
+//			return 1;
+//		}
+		util::cli::ArgumentParser argumentParser;
+		argumentParser.addPositionalArgument(FILENAME_ARG_NAME);
+
+		util::cli::CLIArguments const cliArguments = argumentParser.parseArguments(argc, argv);
 
 		cout << "========== Compilation Started ==========" << endl;
 
-		util::Filename filename(argv[1]);
+		util::Filename filename(cliArguments[FILENAME_ARG_NAME]);
 
 		ScopedFileStream<std::ifstream> inputFileStream(filename.asString(), std::ifstream::in);
 		ANTLRInputStream inputStream(inputFileStream.stream());
@@ -81,6 +91,9 @@ int main(int const argc, char const * const argv[]) {
 		cout << "========== Compilation Finished ==========" << endl;
 
 		return 0;
+	} catch (util::cli::CLIError const &e) {
+		cerr << "CLI Error: " << e.what() << endl;
+		return 2;
 	} catch (std::ios_base::failure const &f) {
         cerr << "Error: Caught an ios_base::failure." << endl;
         cerr << "Error Message: " << f.what() << endl;
