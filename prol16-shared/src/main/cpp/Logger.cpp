4cda95c13647393b66a63744a77db0600b0066f0
creiterer <cr.devel0@gmail.com>
2019-08-06 09:57:04 +0200

    Resolves #58: Implement a logger class

---
+++ prol16-shared/src/main/cpp/Logger.cpp
@@ -0,0 +1,82 @@
+/**
+ * @author		creiterer
+ * @date 		2019-08-05
+ * @copyright 	Copyright (c) 2019 Christopher Reiterer
+ * @brief 		brief description
+ */
+
+#include "Logger.h"
+
+#include <utility>
+#include <algorithm>
+
+namespace util { namespace logging {
+
+Logger::Logger(LogStream logStream, bool const enabled) : logStreams{logStream}, enabled(enabled) {}
+
+Logger::Logger(LogStreams const &logStreams, bool const enabled) : logStreams(logStreams), enabled(enabled) {}
+
+Logger::Logger(LogStreams &&logStreams, bool const enabled) : logStreams(std::move(logStreams)), enabled(enabled) {}
+
+Logger::Logger(std::initializer_list<std::reference_wrapper<std::ostream>> logStreams, bool const enabled) : logStreams(logStreams), enabled(enabled) {}
+
+void Logger::forEachLogStream(std::function<void(LogStream)> const &function) {
+	if (enabled) {
+		std::for_each(logStreams.begin(), logStreams.end(), function);
+	}
+}
+
+/*
+Logger& Logger::operator<<(short const value) {
+
+}
+
+Logger& Logger::operator<<(unsigned short const value) {
+
+}
+
+Logger& Logger::operator<<(int const value) {
+
+}
+
+Logger& Logger::operator<<(unsigned int const value) {
+
+}
+
+Logger& Logger::operator<<(long const value) {
+
+}
+Logger& Logger::operator<<(unsigned long const value) {
+
+}
+
+Logger& Logger::operator<<(long long const value) {
+
+}
+
+Logger& Logger::operator<<(unsigned long long const value) {
+
+}
+
+Logger& Logger::operator<<(float const value) {
+
+}
+
+Logger& Logger::operator<<(double const value) {
+
+}
+
+Logger& Logger::operator<<(long double const value) {
+
+}
+
+Logger& Logger::operator<<(bool const value) {
+
+}
+
+Logger& Logger::operator<<(void const * const value) {
+
+}
+*/
+
+}}
---
+++ prol16-shared/src/main/cpp/Logger.h
@@ -0,0 +1,74 @@
+/**
+ * @author		creiterer
+ * @date 		2019-08-05
+ * @copyright 	Copyright (c) 2019 Christopher Reiterer
+ * @brief 		brief description
+ */
+
+#ifndef PROL16_TOOLS_PROL16_SHARED_SRC_MAIN_CPP_LOGGER_H_INCLUDED
+#define PROL16_TOOLS_PROL16_SHARED_SRC_MAIN_CPP_LOGGER_H_INCLUDED
+
+#include <ostream>
+#include <vector>
+#include <initializer_list>
+#include <functional>
+
+#include "NonCopyable.h"
+
+namespace util { namespace logging {
+
+class Logger final : private NonCopyable {
+public:
+	// since pure references are not assignable, they cannot be used as element type for vectors -> use std::reference_wrapper
+	// see https://stackoverflow.com/questions/23488326/stdvector-of-references/23488449
+	// see https://stackoverflow.com/questions/922360/why-cant-i-make-a-vector-of-references
+	using LogStream = std::ostream&;
+	using LogStreams = std::vector<std::reference_wrapper<std::ostream>>;
+
+	Logger(LogStream logStream, bool const enabled = true);
+	Logger(LogStreams const &logStreams, bool const enabled = true);
+	Logger(LogStreams &&logStreams, bool const enabled = true);
+	Logger(std::initializer_list<std::reference_wrapper<std::ostream>> logStreams, bool const enabled = true);
+
+	inline void addLogStream(LogStream logStream) { logStreams.emplace_back(logStream); }
+
+	inline void setEnabled(bool const enabled) { this->enabled = enabled; }
+	inline void enable() { enabled = true; }
+	inline void disable() { enabled = false; }
+
+	void forEachLogStream(std::function<void(LogStream)> const &function);
+
+	template <typename T>
+	Logger& operator<<(T const &value) {
+		if (enabled) {
+			for (LogStream logStream : logStreams) {
+				logStream << value;
+			}
+		}
+
+		return *this;
+	}
+
+	// https://en.cppreference.com/w/cpp/io/basic_ostream/operator_ltlt
+//	Logger& operator<<(short const value);
+//	Logger& operator<<(unsigned short const value);
+//	Logger& operator<<(int const value);
+//	Logger& operator<<(unsigned int const value);
+//	Logger& operator<<(long const value);
+//	Logger& operator<<(unsigned long const value);
+//	Logger& operator<<(long long const value);
+//	Logger& operator<<(unsigned long long const value);
+//	Logger& operator<<(float const value);
+//	Logger& operator<<(double const value);
+//	Logger& operator<<(long double const value);
+//	Logger& operator<<(bool const value);
+//	Logger& operator<<(void const * const value);
+
+private:
+	LogStreams logStreams;
+	bool enabled;
+};
+
+}}
+
+#endif /* PROL16_TOOLS_PROL16_SHARED_SRC_MAIN_CPP_LOGGER_H_INCLUDED */
