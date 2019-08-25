755cd6ae610b1f3fa7437f525d1678ed3be40dd0
creiterer <cr.devel0@gmail.com>
2019-08-25 10:38:06 +0200

    #75: Fix clang-tidy errors
    * LabelListener

--- prol16-as/src/main/cpp/LabelListener.cpp
+++ prol16-as/src/main/cpp/LabelListener.cpp
@@ -10,108 +10,109 @@
 #include "ContextUtils.h"
 #include "NotImplementedError.h"
 
+// NOLINTNEXTLINE(readability-identifier-naming)
 namespace PROL16 {
 
 void LabelListener::enterLabelStatement(Prol16AsmParser::LabelStatementContext *context) {
 	auto result = labelTable.emplace(context->identifier->getText(), commandCounter.getNextInstructionAddress());
 	if (!result.second) {
-		// TODO: print error that the label is already defined (multiple definition)
+		// TODO(creiterer): print error that the label is already defined (multiple definition)
 	}
 }
 
-void LabelListener::enterNopInstruction(Prol16AsmParser::NopInstructionContext *context) {
+void LabelListener::enterNopInstruction(Prol16AsmParser::NopInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterSleepInstruction(Prol16AsmParser::SleepInstructionContext *context) {
+void LabelListener::enterSleepInstruction(Prol16AsmParser::SleepInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterLoadiInstruction(Prol16AsmParser::LoadiInstructionContext *context) {
+void LabelListener::enterLoadiInstruction(Prol16AsmParser::LoadiInstructionContext */*context*/) {
 	commandCounter += 2;	// 'loadi' takes the space of two instructions: the instruction itself and the immediate
 }
 
-void LabelListener::enterLoadInstruction(Prol16AsmParser::LoadInstructionContext *context) {
+void LabelListener::enterLoadInstruction(Prol16AsmParser::LoadInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterStoreInstruction(Prol16AsmParser::StoreInstructionContext *context) {
+void LabelListener::enterStoreInstruction(Prol16AsmParser::StoreInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterMoveInstruction(Prol16AsmParser::MoveInstructionContext *context) {
+void LabelListener::enterMoveInstruction(Prol16AsmParser::MoveInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterJumpInstruction(Prol16AsmParser::JumpInstructionContext *context) {
+void LabelListener::enterJumpInstruction(Prol16AsmParser::JumpInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterJumpcInstruction(Prol16AsmParser::JumpcInstructionContext *context) {
+void LabelListener::enterJumpcInstruction(Prol16AsmParser::JumpcInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterJumpzInstruction(Prol16AsmParser::JumpzInstructionContext *context) {
+void LabelListener::enterJumpzInstruction(Prol16AsmParser::JumpzInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterAndInstruction(Prol16AsmParser::AndInstructionContext *context) {
+void LabelListener::enterAndInstruction(Prol16AsmParser::AndInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterOrInstruction(Prol16AsmParser::OrInstructionContext *context) {
+void LabelListener::enterOrInstruction(Prol16AsmParser::OrInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterXorInstruction(Prol16AsmParser::XorInstructionContext *context) {
+void LabelListener::enterXorInstruction(Prol16AsmParser::XorInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterNotInstruction(Prol16AsmParser::NotInstructionContext *context) {
+void LabelListener::enterNotInstruction(Prol16AsmParser::NotInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterAddInstruction(Prol16AsmParser::AddInstructionContext *context) {
+void LabelListener::enterAddInstruction(Prol16AsmParser::AddInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterAddcInstruction(Prol16AsmParser::AddcInstructionContext *context) {
+void LabelListener::enterAddcInstruction(Prol16AsmParser::AddcInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterSubInstruction(Prol16AsmParser::SubInstructionContext *context) {
+void LabelListener::enterSubInstruction(Prol16AsmParser::SubInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterSubcInstruction(Prol16AsmParser::SubcInstructionContext *context) {
+void LabelListener::enterSubcInstruction(Prol16AsmParser::SubcInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterCompInstruction(Prol16AsmParser::CompInstructionContext *context) {
+void LabelListener::enterCompInstruction(Prol16AsmParser::CompInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterIncInstruction(Prol16AsmParser::IncInstructionContext *context) {
+void LabelListener::enterIncInstruction(Prol16AsmParser::IncInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterDecInstruction(Prol16AsmParser::DecInstructionContext *context) {
+void LabelListener::enterDecInstruction(Prol16AsmParser::DecInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterShlInstruction(Prol16AsmParser::ShlInstructionContext *context) {
+void LabelListener::enterShlInstruction(Prol16AsmParser::ShlInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterShrInstruction(Prol16AsmParser::ShrInstructionContext *context) {
+void LabelListener::enterShrInstruction(Prol16AsmParser::ShrInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterShlcInstruction(Prol16AsmParser::ShlcInstructionContext *context) {
+void LabelListener::enterShlcInstruction(Prol16AsmParser::ShlcInstructionContext */*context*/) {
 	++commandCounter;
 }
 
-void LabelListener::enterShrcInstruction(Prol16AsmParser::ShrcInstructionContext *context) {
+void LabelListener::enterShrcInstruction(Prol16AsmParser::ShrcInstructionContext */*context*/) {
 	++commandCounter;
 }
 
@@ -127,4 +128,4 @@ void LabelListener::enterPrintInstruction(Prol16AsmParser::PrintInstructionConte
 	}
 }
 
-}
+} 	// namespace PROL16
--- prol16-as/src/main/cpp/LabelListener.h
+++ prol16-as/src/main/cpp/LabelListener.h
@@ -8,13 +8,14 @@
 #ifndef PROL16_ASM_PROL16_AS_SRC_MAIN_CPP_LABELLISTENER_H_INCLUDED
 #define PROL16_ASM_PROL16_AS_SRC_MAIN_CPP_LABELLISTENER_H_INCLUDED
 
+#include "CommandCounter.h"
+#include "NumberUtils.h"
+#include "Prol16AsmParserBaseListener.h"
+
 #include <string>
 #include <unordered_map>
 
-#include "Prol16AsmParserBaseListener.h"
-#include "CommandCounter.h"
-#include "NumberUtils.h"
-
+// NOLINTNEXTLINE(readability-identifier-naming)
 namespace PROL16 {
 
 class LabelListener final : public Prol16AsmParserBaseListener {
@@ -24,57 +25,56 @@ public:
 	using LabelTable = std::unordered_map<LabelName, LabelValue>;
 
 	LabelListener() = default;
-	~LabelListener() = default;
 
-	void enterLabelStatement(Prol16AsmParser::LabelStatementContext *context) override;
+	void enterLabelStatement(Prol16AsmParser::LabelStatementContext */*context*/) override;
 
-	void enterNopInstruction(Prol16AsmParser::NopInstructionContext *context) override;
+	void enterNopInstruction(Prol16AsmParser::NopInstructionContext */*context*/) override;
 
-	void enterSleepInstruction(Prol16AsmParser::SleepInstructionContext *context) override;
+	void enterSleepInstruction(Prol16AsmParser::SleepInstructionContext */*context*/) override;
 
-	void enterLoadiInstruction(Prol16AsmParser::LoadiInstructionContext *context) override;
+	void enterLoadiInstruction(Prol16AsmParser::LoadiInstructionContext */*context*/) override;
 
-	void enterLoadInstruction(Prol16AsmParser::LoadInstructionContext *context) override;
+	void enterLoadInstruction(Prol16AsmParser::LoadInstructionContext */*context*/) override;
 
-	void enterStoreInstruction(Prol16AsmParser::StoreInstructionContext *context) override;
+	void enterStoreInstruction(Prol16AsmParser::StoreInstructionContext */*context*/) override;
 
-	void enterMoveInstruction(Prol16AsmParser::MoveInstructionContext *context) override;
+	void enterMoveInstruction(Prol16AsmParser::MoveInstructionContext */*context*/) override;
 
-	void enterJumpInstruction(Prol16AsmParser::JumpInstructionContext *context) override;
+	void enterJumpInstruction(Prol16AsmParser::JumpInstructionContext */*context*/) override;
 
-	void enterJumpcInstruction(Prol16AsmParser::JumpcInstructionContext *context) override;
+	void enterJumpcInstruction(Prol16AsmParser::JumpcInstructionContext */*context*/) override;
 
-	void enterJumpzInstruction(Prol16AsmParser::JumpzInstructionContext *context) override;
+	void enterJumpzInstruction(Prol16AsmParser::JumpzInstructionContext */*context*/) override;
 
-	void enterAndInstruction(Prol16AsmParser::AndInstructionContext *context) override;
+	void enterAndInstruction(Prol16AsmParser::AndInstructionContext */*context*/) override;
 
-	void enterOrInstruction(Prol16AsmParser::OrInstructionContext *context) override;
+	void enterOrInstruction(Prol16AsmParser::OrInstructionContext */*context*/) override;
 
-	void enterXorInstruction(Prol16AsmParser::XorInstructionContext *context) override;
+	void enterXorInstruction(Prol16AsmParser::XorInstructionContext */*context*/) override;
 
-	void enterNotInstruction(Prol16AsmParser::NotInstructionContext *context) override;
+	void enterNotInstruction(Prol16AsmParser::NotInstructionContext */*context*/) override;
 
-	void enterAddInstruction(Prol16AsmParser::AddInstructionContext *context) override;
+	void enterAddInstruction(Prol16AsmParser::AddInstructionContext */*context*/) override;
 
-	void enterAddcInstruction(Prol16AsmParser::AddcInstructionContext *context) override;
+	void enterAddcInstruction(Prol16AsmParser::AddcInstructionContext */*context*/) override;
 
-	void enterSubInstruction(Prol16AsmParser::SubInstructionContext *context) override;
+	void enterSubInstruction(Prol16AsmParser::SubInstructionContext */*context*/) override;
 
-	void enterSubcInstruction(Prol16AsmParser::SubcInstructionContext *context) override;
+	void enterSubcInstruction(Prol16AsmParser::SubcInstructionContext */*context*/) override;
 
-	void enterCompInstruction(Prol16AsmParser::CompInstructionContext *context) override;
+	void enterCompInstruction(Prol16AsmParser::CompInstructionContext */*context*/) override;
 
-	void enterIncInstruction(Prol16AsmParser::IncInstructionContext *context) override;
+	void enterIncInstruction(Prol16AsmParser::IncInstructionContext */*context*/) override;
 
-	void enterDecInstruction(Prol16AsmParser::DecInstructionContext *context) override;
+	void enterDecInstruction(Prol16AsmParser::DecInstructionContext */*context*/) override;
 
-	void enterShlInstruction(Prol16AsmParser::ShlInstructionContext *context) override;
+	void enterShlInstruction(Prol16AsmParser::ShlInstructionContext */*context*/) override;
 
-	void enterShrInstruction(Prol16AsmParser::ShrInstructionContext *context) override;
+	void enterShrInstruction(Prol16AsmParser::ShrInstructionContext */*context*/) override;
 
-	void enterShlcInstruction(Prol16AsmParser::ShlcInstructionContext *context) override;
+	void enterShlcInstruction(Prol16AsmParser::ShlcInstructionContext */*context*/) override;
 
-	void enterShrcInstruction(Prol16AsmParser::ShrcInstructionContext *context) override;
+	void enterShrcInstruction(Prol16AsmParser::ShrcInstructionContext */*context*/) override;
 
 	void enterPrintInstruction(Prol16AsmParser::PrintInstructionContext *context) override;
 
@@ -85,6 +85,6 @@ private:
 	LabelTable labelTable;
 };
 
-}
+}	// namespace PROL16
 
 #endif /* PROL16_ASM_PROL16_AS_SRC_MAIN_CPP_LABELLISTENER_H_INCLUDED */
