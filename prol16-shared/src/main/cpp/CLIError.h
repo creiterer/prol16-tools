c4abece0fb260957a5d4f75a56ede00e9a9f86ad
creiterer <cr.devel0@gmail.com>
2019-08-25 10:46:20 +0200

    Issue #75: Fix clang-tidy errors
    * CLIError

--- prol16-shared/src/main/cpp/CLIError.h
+++ prol16-shared/src/main/cpp/CLIError.h
@@ -15,10 +15,11 @@ namespace util { namespace cli {
 
 class CLIError final : public std::runtime_error {
 public:
-	CLIError(std::string const &message) : runtime_error(message) {}
+	explicit CLIError(std::string const &message) : runtime_error(message) {}
 	CLIError(std::string const &error, std::string const &usage) : runtime_error(error + "\n" + usage) {}
 };
 
-}}
+}	// namespace cli
+}	// namespace util
 
 #endif /* PROL16_ASM_PROL16_SHARED_SRC_MAIN_CPP_CLIERROR_H_INCLUDED */
