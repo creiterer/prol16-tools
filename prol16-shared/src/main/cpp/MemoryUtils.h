0bb42ae07775a4c761ee24ae91caa2c8622c24f5
creiterer <cr.devel0@gmail.com>
2019-10-11 09:16:48 +0200

    Issue #103: added type for memory segments

--- prol16-shared/src/main/cpp/MemoryUtils.h
+++ prol16-shared/src/main/cpp/MemoryUtils.h
@@ -10,12 +10,14 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 
 // NOLINTNEXTLINE(readability-identifier-naming)
 namespace PROL16 { namespace util { namespace memory {
 
 using Data = uint16_t;
 using Address = uint16_t;
+using Segment = std::vector<Data>;
 
 unsigned calcStringSpaceInMemory(std::string const &str);
 
